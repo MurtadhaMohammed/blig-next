dcf35e93cea0b1f01abd4ac524b3e2868b6f9876
MurtadhaMohammed <mumu9943@gmail.com>
2020-09-13 20:02:00 +0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import '../css/style.css'
+import '../css/responsive.css'
+import '../css/animation.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
